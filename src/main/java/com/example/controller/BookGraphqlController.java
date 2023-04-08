f5a72ab4b08759bc999e95499fcb08b8f9c78538
Dk2698 <dileepkumar1823@gmail.com>
2023-04-08 15:27:13 +0530

    graphql with auery  mutation

--- src/main/java/com/example/controller/BookGraphqlController.java
+++ src/main/java/com/example/controller/BookGraphqlController.java
@@ -2,8 +2,11 @@ package com.example.controller;
 
 import com.example.entity.Book;
 import com.example.service.BookService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +20,15 @@ public class BookGraphqlController {
     @Autowired
     private BookService  bookService;
 
-    public Book create(@RequestBody Book book){
-        return this.bookService.crate(book);
+    @MutationMapping("createBook")
+    public Book create(@Argument BookInput book){
+        Book b = new Book();
+        b.setTitle(book.getTitle());
+        b.setDesc(book.getDesc());
+        b.setPrice(book.getPrice());
+        b.setAuthor(book.getAuthor());
+        b.setPrice(book.getPages());
+        return this.bookService.crate(b);
     }
 
     @QueryMapping("allBooks")
@@ -30,4 +40,14 @@ public class BookGraphqlController {
     public Book get(@Argument int bookId){
         return this.bookService.get(bookId);
     }
+}
+
+@Getter
+@Setter
+class BookInput{
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
 }
